fccc4e809a193d04839c16d9cfb4e7e91e28a553
Lisina <lisina.pro@gmail.com>
2025-08-17 17:35:15 +0300

    chanch main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int remains = 100;
+        int payment = 1100;
+
+        int bonus = payment / 100;
+        if (payment < 1000) {
+            bonus = 0;
+        } else {
+            bonus = payment/100;
+        }
+        int balance = payment + bonus + remains;
+        System.out.println("Сумма на счете:" + balance);
+
+    }
+}
