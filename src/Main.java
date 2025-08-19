631368acc2c13764e5e94e47717874ad79dad2b5
Lisina <lisina.pro@gmail.com>
2025-08-19 21:48:12 +0300

    main new

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
         int payment = 1100;
 
         int bonus = payment / 100;
-        if (payment < 1000) {
+        if (payment < 1001) {
             bonus = 0;
         } else {
-            bonus = payment / 100;
+
         }
         int balance = payment + bonus + remains;
         System.out.println("Сумма на счете:" + balance);
